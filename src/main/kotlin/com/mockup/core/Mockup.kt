b4675b9fbd44ecbedb4e46eab1dff8b011fba9f7
Miroslav Hýbler <miroslav@peko-studio.cz>
2026-01-10 22:18:14 +0100

    Generation to module package instead of hadcoded "com.mockup", added value getter functions to Mockup.kt, example-app - package changed to com.example.app

--- src/main/kotlin/com/mockup/core/Mockup.kt
+++ src/main/kotlin/com/mockup/core/Mockup.kt
@@ -18,10 +18,99 @@ import kotlin.reflect.full.primaryConstructor
 public object Mockup {
 
     /**
-     * Package name for generated mockup providers.
+     * Returns the first element from the generated sequence of the mockup data for the given type [T].
+     *
+     * @return The first element from the sequence.
+     * @throws IllegalArgumentException if the provider for the given type is not found.
      * @since 2.0.0
      */
-    internal const val PROVIDER_PACKAGE_NAME = "com.mockup.providers"
+    public inline fun <reified T : Any> get(): T = getProvider(clazz = T::class.java).first
+
+    /**
+     * Returns the first element from the generated sequence of the mockup data for the given type [T], or `null` if the provider is not found.
+     *
+     * @return The first element from the sequence, or `null`.
+     * @since 2.0.0
+     */
+    public inline fun <reified T : Any> getOrNull(): T? =
+        getProviderOrNull(clazz = T::class.java)?.first
+
+    /**
+     * Returns a random element from the generated sequence of the mockup data for the given type [T].
+     *
+     * @return A random element from the sequence.
+     * @throws IllegalArgumentException if the provider for the given type is not found.
+     * @since 2.0.0
+     */
+    public inline fun <reified T : Any> getRandom(): T =
+        getProvider(clazz = T::class.java).random
+
+    /**
+     * Returns a random element from the generated sequence of the mockup data for the given type [T], or `null` if the provider is not found.
+     *
+     * @return A random element from the sequence, or `null`.
+     * @since 2.0.0
+     */
+    public inline fun <reified T : Any> getRandomOrNull(): T? =
+        getProviderOrNull(clazz = T::class.java)?.random
+
+
+    /**
+     * Returns the whole sequence of the generated mockup data for the given type [T] as a [List].
+     *
+     * @return The list of generated mockup data.
+     * @throws IllegalArgumentException if the provider for the given type is not found.
+     * @since 2.0.0
+     */
+    public inline fun <reified T : Any> getList(): List<T> =
+        getProvider(clazz = T::class.java).list
+
+
+    /**
+     * Returns the whole sequence of the generated mockup data for the given type [T] as a [List], or `null` if the provider is not found.
+     *
+     * @return The list of generated mockup data, or `null`.
+     * @since 2.0.0
+     */
+    public inline fun <reified T : Any> getListOrNull(): List<T>? =
+        getProviderOrNull(clazz = T::class.java)?.list
+
+
+    /**
+     * @param json Json string to be parsed.
+     * @return Parsed object of type [T].
+     * @throws SerializationException in case of any decoding-specific error
+     * @throws IllegalArgumentException if the decoded input is not a valid instance of [T]
+     * @since 2.0.0
+     */
+    @Throws(
+        exceptionClasses = [
+            SerializationException::class,
+            IllegalArgumentException::class
+        ]
+    )
+    public inline fun <reified T : Any> fromJson(
+        json: String
+    ): T {
+        return Json.decodeFromString<T>(string = json)
+    }
+
+
+    /**
+     * @param json Json string to be parsed.
+     * @return Parsed object of type [T] or null when [json] cannot be parsed into the type.
+     * @since 2.0.0
+     */
+    public inline fun <reified T : Any> fromJsonOrNull(
+        json: String
+    ): T? {
+        return try {
+            fromJson<T>(json = json)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 
 
     /**
@@ -35,7 +124,7 @@ public object Mockup {
      * @since 2.0.0
      */
     public fun <T : Any> getProvider(clazz: Class<T>): MockupDataProvider<T> {
-        val className = "$PROVIDER_PACKAGE_NAME.${clazz.simpleName}MockupProvider"
+        val className = "${clazz.name}MockupProvider"
         try {
             val providerClass = Class.forName(className).kotlin
             val providerInstance = providerClass.primaryConstructor?.call()
@@ -53,6 +142,7 @@ public object Mockup {
             )
         }
     }
+
 
     /**
      * Returns the generated [MockupDataProvider] for the given type [T].
@@ -97,41 +187,5 @@ public object Mockup {
     public inline fun <reified T : Any> getProviderOrNull(): MockupDataProvider<T>? =
         getProviderOrNull(clazz = T::class.java)
 
-
-    /**
-     * @param json Json string to be parsed.
-     * @return Parsed object of type [T].
-     * @throws SerializationException in case of any decoding-specific error
-     * @throws IllegalArgumentException if the decoded input is not a valid instance of [T]
-     * @since 2.0.0
-     */
-    @Throws(
-        exceptionClasses = [
-            SerializationException::class,
-            IllegalArgumentException::class
-        ]
-    )
-    public inline fun <reified T : Any> fromJson(
-        json: String
-    ): T {
-        return Json.decodeFromString<T>(string = json)
-    }
-
-
-    /**
-     * @param json Json string to be parsed.
-     * @return Parsed object of type [T] or null when [json] cannot be parsed into the type.
-     * @since 2.0.0
-     */
-    public inline fun <reified T : Any> fromJsonOrNull(
-        json: String
-    ): T? {
-        return try {
-            fromJson<T>(json = json)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
 
 }
